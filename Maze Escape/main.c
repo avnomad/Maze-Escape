4e467fcfc0c63c7d5a3666b49b5696f6744da1fe
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-27 18:28:35 +0300

    Added copyright notices and license.

--- Maze Escape/main.c
+++ Maze Escape/main.c
@@ -1,3 +1,21 @@
+//	Copyright (C) 2006, 2008, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Maze Escape.
+ *
+ *	Maze Escape is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Maze Escape is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Maze Escape.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -182,7 +200,7 @@ void find_exit(cell current,cell direction,char *maze,char maxcol,char maxrow)
 				putc(*(maze+row*maxcol+col),outputfile);
 			putc('\n',outputfile);
 		}
-		printf("The path to the exit was found and is saved in the file \'maze_exit.txt\'.\n");
+		printf("The path to the exit was found and saved in the file \'maze_exit.txt\'.\n");
 		fclose(outputfile);
 		system("PAUSE");
 		exit(0);
